11d76c51a50a713081ed41a548c54c3c8dd616cd
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-06-19 14:32:14 +0700

    fix backend

--- api/index.js
+++ api/index.js
@@ -11,6 +11,38 @@ app.get('/api/products', async (c) => {
   return c.json(results)
 })
 
+app.post('/api/products', async (c) => {
+  const input = await c.req.json()
+  const query = `INSERT INTO products(name,price) values ("${input.name}","${input.price}")`
+  const newData = await c.env.DB.exec(query)
+  return c.json(newData)
+})
+
+app.get('/api/products/:id', async (c) => {
+  const id = c.req.param('id')
+  let { results } = await c.env.DB.prepare('SELECT * FROM products WHERE id = ?').bind(id).all()
+  return c.json(results[0])
+})
+
+app.put('/api/products/:id', async (c) => {
+  const id = c.req.param('id')
+
+  const input = await c.req.json()
+  const query = `UPDATE products SET name = "${input.name}", price = "${input.price}" WHERE id = "${id}"`
+  const data = await c.env.DB.exec(query)
+
+  return c.json(data)
+})
+
+app.delete('/api/products/:id', async (c) => {
+  const id = c.req.param('id')
+
+  const query = `DELETE FROM products WHERE id = "${id}"`
+  const data = await c.env.DB.exec(query)
+
+  return c.json(data)
+})
+
 app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
 
 export default app;
